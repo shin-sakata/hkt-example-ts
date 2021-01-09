59d0593913e2fb6714df31bee3a1f87afa980d0f
shin-sakata <shintaro.sakata.tokyo@gmail.com>
2021-01-09 11:13:53 +0900

    move

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
-import { map } from './Functor'
-import { Identity } from './Identity'
-import { some, Option } from './Option'
+import { map } from './Objective/Functor'
+import { Identity } from './Objective/Identity'
+import { some, Option } from './Objective/Option'
 
 let x = some(1)
 let y = new Identity(1)
 const double = (n: number) => n * 2
 
+// map関数がジェネリックになってる。
 let result1: Option<number> = map(x, double)
-
-let result2 = map(y, double)
+let result2: Identity<number> = map(y, double)
 
 console.log(x.map(double))
 console.log(map(x, double))
